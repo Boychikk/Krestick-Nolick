0a07db722d7dd94a1e59d98869ae0da0a1d959c2
KIRILL VOROBEV <kudikyy@gmail.com>
2024-09-14 03:40:03 +0700

    пососи

--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,36 @@
 <script setup lang="ts">
-const id = ref("");
-const content = ref("");
-const fielddata = ref(null);
+interface FieldData {
+  id: number | string,
+  content: string,
+}
+
+const fielddata = ref<FieldData[]>([]);
 const loading = ref(true);
-const {data} = await useFetch(`https://express-sb.vercel.app/get-field-data`);
 onMounted(async () => {
-  console.log(data.value)
-  fielddata.value = data.value?.data ?? [];
+  await getFieldData();
   loading.value = false;
 });
-const setValue = async () => {
+
+const getSymbol = computed(() => {
+  let xAmount = 0;
+  let oAmount = 0;
+  for (const item of fielddata.value) {
+    if (item.content.toLowerCase() === "x") xAmount += 1
+    else if (item.content.toLowerCase() === "0") oAmount += 1;
+  }
+  return xAmount === oAmount ? "x" : "0";
+})
+
+const getFieldData = async () => {
+  const result = await $fetch<{ data: FieldData[] }>(`https://express-sb.vercel.app/get-field-data`)
+  fielddata.value = result.data
+  setTimeout(getFieldData, 1500)
+}
+
+const setValue = async (id: number | string) => {
   const newEl = {
-    id: id.value,
-    content: content.value
+    id,
+    content: getSymbol.value,
   }
   await $fetch("https://express-sb.vercel.app/set-field-value", {
     method: "POST",
@@ -22,22 +40,21 @@ const setValue = async () => {
   fielddata.value[fielddata.value.findIndex(el => el.id == newEl.id)] = newEl;
 }
 
+const resetField = async () => {
+  await $fetch("https://express-sb.vercel.app/reset-game", { method: "POST" })
+}
 </script>
 
 <template>
   <div class="center">
-    <button @click="setValue">Рестарт</button>
+    <button @click="resetField">Рестарт</button>
     <div v-if="loading">Loading...</div>
     <div v-else class="grid">
-      <div class="square" v-for="i in fielddata" :key="i.id" @click="id = i.id">
+      <div class="square" v-for="i in fielddata" :key="i.id" @click="setValue(i.id)">
         {{ i.content }}
       </div>
     </div>
-    <input v-model="id" />
-    <input v-model="content" />
-
   </div>
-
 </template>
 
 <style>
